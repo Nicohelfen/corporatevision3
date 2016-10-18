f6f34f41652d002eb1f33d19efce7f5d636742ab
nh <nhelfenberger@vev.fr>
2016-10-18 15:33:26 +0200

    pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,33 @@
 class PagesController < ApplicationController
 
   def home
+    @links = ['approche', 'page2', 'page3', 'philosophie', 'clients', 'contact']
   end
+
+
+  def page1
+
+  end
+
+  def page2
+
+  end
+
+  def page3
+
+  end
+
+  def page4
+
+  end
+
+  def page5
+
+  end
+
+
+  def page6
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
-
+  root to: 'pages#home'
+    get "approche" => 'pages#page1'
+    get "page2" => 'pages#page2'
+    get "page3" => 'pages#page3'
+    get "philosophie" => 'pages#page4'
+    get "clients" => 'pages#page5'
+    get "contact" => 'pages#page6'
 end
