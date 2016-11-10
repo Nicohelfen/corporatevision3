a55d50055954e40e804c9166c548a379b1003f37
nh <nhelfenberger@vev.fr>
2016-11-10 09:21:51 +0100

    modifroute

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
 private
   def page_title
-    @links = [['digital-learning', 'Digital Learning'], ['plateforme-lms', 'Plateforme LMS'], ['expertise-santé-sécurité', 'Expertise Santé Sécurité'], ['vision3', 'Vision3'], ['clients', 'Clients'], ['contact', 'Contact']]
+    @links = [['digital-learning', 'Digital Learning'], ['plateforme-lms', 'Plateforme LMS'], ['expertise-sante-securite', 'Expertise Santé Sécurité'], ['vision3', 'Vision3'], ['clients', 'Clients'], ['contact', 'Contact']]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
     get "digital-learning" => 'pages#page1'
     get "plateforme-lms" => 'pages#page2'
-    get "expertise-santé-sécurité" => 'pages#page3'
+    get "expertise-sante-securite" => 'pages#page3'
     get "vision3" => 'pages#page4'
     get "clients" => 'pages#page5'
     get "contact" => 'pages#page6'
